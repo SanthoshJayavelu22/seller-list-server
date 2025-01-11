161360c26e66415171919d9cbc5d220482e5f22a
Santhosh Jayavelu <127133708+SanthoshJayavelu22@users.noreply.github.com>
2025-01-11 16:36:59 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -31,7 +31,7 @@ const sampleSellers = [
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/seller", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
